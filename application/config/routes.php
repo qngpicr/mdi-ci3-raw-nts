b785cb84b2e71e9eb902a4fb20d788b9f4e8ca59
qngpicr <qngpicr@gmail.com>
2026-01-12 02:39:44 +0900

    main-260112-0239

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,3 +5,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['home'] = 'home/index';
+$route['index'] = 'home/index';
